51e91e8936d25f2e83a70ab0f6dc35b701a09825
typewriter <takuyaxp@gmail.com>
2022-01-28 21:10:41 +0900

    Send dummy udp packet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::UdpSocket;
+
 fn main() {
     println!("Hello, world!");
+
+    let socket = UdpSocket::bind("0.0.0.0:12345").expect("Couldn't bind to address");
+    let buffer = [1, 2, 3];
+    socket
+        .send_to(&buffer, "192.168.12.1:53")
+        .expect("Couldn't send");
 }
