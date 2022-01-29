1d0aeb64ab069a6dc8dc6465c396dd2630006abb
typewriter <takuyaxp@gmail.com>
2022-01-29 17:07:37 +0900

    Build header

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+mod message;
+
 use std::net::UdpSocket;
 
 fn main() {
+    let message = message::Message {
+        header: message::Header::new(),
+        query: message::Query {},
+    };
+
+    let header = message::Header::create(
+        1000, 0b0, 0b0101, 0b0, 0b0, 0b0, 0b0, 0b000, 0b0101, 1, 0, 0, 0,
+    );
+
+    println!(
+        "QR: {}, OPCODE: {}, AA: {}, TC: {}, RD: {}, RA: {}, Z: {}, RCODE: {}",
+        header.qr(),
+        header.opcode(),
+        header.aa(),
+        header.tc(),
+        header.rd(),
+        header.ra(),
+        header.z(),
+        header.rcode()
+    );
+
     println!("Hello, world!");
 
     let socket = UdpSocket::bind("0.0.0.0:12345").expect("Couldn't bind to address");
-    let buffer = [
-        0x3c, 0x0b, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x77, 0x77,
-        0x77, 0x08, 0x6e, 0x79, 0x61, 0x6d, 0x69, 0x6b, 0x61, 0x6e, 0x03, 0x6e, 0x65, 0x74, 0x00,
-        0x00, 0x01, 0x00, 0x01,
-    ];
+    let buffer = header.to_byte();
+    // let buffer = [
+    //     0x3c, 0x0b, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x77, 0x77,
+    //     0x77, 0x08, 0x6e, 0x79, 0x61, 0x6d, 0x69, 0x6b, 0x61, 0x6e, 0x03, 0x6e, 0x65, 0x74, 0x00,
+    //     0x00, 0x01, 0x00, 0x01,
+    // ];
     socket
         .send_to(&buffer, "192.168.12.1:53")
         .expect("Couldn't send");
---
+++ src/message.rs
@@ -0,0 +1,108 @@
+pub struct Message {
+    pub header: Header,
+    pub query: Query,
+}
+
+pub struct Header {
+    pub id: u16,
+    pub flags: u16,
+    pub qd_count: u16,
+    pub an_count: u16,
+    pub ns_count: u16,
+    pub ar_count: u16,
+}
+
+impl Header {
+    pub fn new() -> Self {
+        Self {
+            id: 1000,
+            flags: 0,
+            qd_count: 1,
+            an_count: 0,
+            ns_count: 0,
+            ar_count: 0,
+        }
+    }
+
+    pub fn create(
+        id: u16,
+        qr: u8,
+        opcode: u8,
+        aa: u8,
+        tc: u8,
+        rd: u8,
+        ra: u8,
+        z: u8,
+        rcode: u8,
+        qd_count: u16,
+        an_count: u16,
+        ns_count: u16,
+        ar_count: u16,
+    ) -> Self {
+        Self {
+            id: id,
+            flags: ((qr.wrapping_shl(7)
+                + opcode.wrapping_shl(3)
+                + aa.wrapping_shl(2)
+                + tc.wrapping_shl(1)
+                + rd) as u16)
+                .wrapping_shl(8)
+                + (ra.wrapping_shl(7) + z.wrapping_shl(4) + rcode) as u16,
+            qd_count: qd_count,
+            an_count: an_count,
+            ns_count: ns_count,
+            ar_count: ar_count,
+        }
+    }
+
+    pub fn qr(&self) -> u8 {
+        (self.flags >> 15 & 0b0001) as u8
+    }
+
+    pub fn opcode(&self) -> u8 {
+        (self.flags >> 11 & 0b1111) as u8
+    }
+
+    pub fn aa(&self) -> u8 {
+        (self.flags >> 10 & 0b0001) as u8
+    }
+
+    pub fn tc(&self) -> u8 {
+        (self.flags >> 9 & 0b0001) as u8
+    }
+
+    pub fn rd(&self) -> u8 {
+        (self.flags >> 8 & 0b0001) as u8
+    }
+
+    pub fn ra(&self) -> u8 {
+        (self.flags >> 7 & 0b0001) as u8
+    }
+
+    pub fn z(&self) -> u8 {
+        (self.flags >> 4 & 0b0111) as u8
+    }
+
+    pub fn rcode(&self) -> u8 {
+        (self.flags & 0b1111) as u8
+    }
+
+    pub fn to_byte(&self) -> [u8; 12] {
+        let mut bytes: [u8; 12] = [0; 12];
+        bytes[0] = (self.id / 256) as u8;
+        bytes[1] = (self.id % 256) as u8;
+        bytes[2] = (self.flags / 256) as u8;
+        bytes[3] = (self.flags % 256) as u8;
+        bytes[4] = (self.qd_count / 256) as u8;
+        bytes[5] = (self.qd_count % 256) as u8;
+        bytes[6] = (self.an_count / 256) as u8;
+        bytes[7] = (self.an_count % 256) as u8;
+        bytes[8] = (self.ns_count / 256) as u8;
+        bytes[9] = (self.ns_count % 256) as u8;
+        bytes[10] = (self.ar_count / 256) as u8;
+        bytes[11] = (self.ar_count % 256) as u8;
+        bytes
+    }
+}
+
+pub struct Query {}
