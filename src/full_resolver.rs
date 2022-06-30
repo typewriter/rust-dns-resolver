2d94c0367ec83472019274c0f0917be20f7523b1
typewriter <takuyaxp@gmail.com>
2022-06-30 17:29:12 +0900

    WIP: Implement first query

--- src/full_resolver.rs
+++ src/full_resolver.rs
@@ -1,3 +1,8 @@
+use core::time;
+use rand::Rng;
+use std::{net::UdpSocket, thread::sleep};
+
+use crate::message;
 
 const ROOT_NAME_SERVERS: [&str; 13] = [
     // See: root-servers.org
@@ -18,4 +23,90 @@ const ROOT_NAME_SERVERS: [&str; 13] = [
 
 pub fn resolve(fqdn: &str, qtype: u16) {
     println!("{:?} の type {:?} を解決していくよ！", fqdn, qtype);
+
+    // ルートサーバ
+    let mut rng = rand::thread_rng();
+    let mut nameserver = ROOT_NAME_SERVERS[rng.gen_range(0..ROOT_NAME_SERVERS.len())];
+
+    // 以下の条件に達するまでクエリを投げ続ける
+    // - Answer が得られる
+    // - RCODE が 0 以外で何らかのエラーが生じている
+    let mut id = 1;
+    loop {
+        id = id + 1;
+
+        let message = message::Message {
+            header: message::Header::create(
+                id, 0b0, 0b0000, 0b0, 0b0, 0b0, 0b0, 0b000, 0b0000, 0x0001, 0x0000, 0x0000, 0x0000,
+            ),
+            question: message::Question::new(fqdn, qtype, 0x0001),
+        };
+
+        println!("{:?} に問い合わせます...", nameserver);
+        sleep(time::Duration::from_millis(2000));
+
+        let socket = UdpSocket::bind("0.0.0.0:12345").expect("Couldn't bind to address");
+        let buffer = message.to_bytes();
+        let buffer_slice = buffer.as_slice();
+        socket
+            .send_to(&buffer_slice, [nameserver, ":53"].concat())
+            .expect("Couldn't send");
+
+        // Receive
+        let mut buf = [0; 512];
+        let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
+        println!(
+            "number_of_bytes: {}, src_addr: {:?}",
+            number_of_bytes, src_addr
+        );
+        println!("{:?}", buf);
+
+        // Response
+        let mut ret_header: [u8; 12] = Default::default();
+        ret_header.copy_from_slice(&buf[0..12]);
+        let ret_header = message::Header::parse(&ret_header);
+
+        let ret_body = &buf[12..512];
+        let questions = message::Question::parse(&ret_body, ret_header.qd_count.into());
+        println!("{:?}", questions.0);
+
+        let ret_body = &buf[(questions.1 + 12)..512];
+        let resources = message::Resource::parse(
+            &buf,
+            ret_body,
+            (ret_header.an_count + ret_header.ns_count + ret_header.ar_count).into(),
+        );
+
+        println!("{:?}", resources);
+
+        // 判定
+        if ret_header.an_count > 0 {
+            println!("結果が得られました。終了します");
+            break;
+        }
+        if ret_header.rcode() > 0 {
+            println!(
+                "エラーが返されました (RCODE: {:?}) 。終了します",
+                ret_header.rcode()
+            );
+            break;
+        }
+
+        println!("ここに答えはありませんでした。次の問い合わせ先を探します");
+
+        // 次の問い合わせ先を探す
+        let begin = (ret_header.qd_count + ret_header.an_count) as usize;
+        let end = begin + (ret_header.ns_count - 1) as usize;
+        let mut ns_records = &resources[begin..end];
+
+        let begin = (ret_header.qd_count + ret_header.an_count + ret_header.ns_count) as usize;
+        let end = begin + (ret_header.ar_count - 1) as usize;
+        let mut ar_records = &resources[begin..end];
+
+        println!("NS records: {:?}", ns_records);
+        println!("AR records: {:?}", ar_records);
+
+        // FIXME: 未実装
+        break;
+    }
 }
